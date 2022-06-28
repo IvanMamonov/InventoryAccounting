42bccb5f1e84fea700d1cccd884b23f321bf1d34
Иван Мамонов <mamonov_vanya03@mail.ru>
2022-06-28 11:36:05 +0300

    dfh

--- InventoryAccounting/dbo/Accounting.Designer.cs
+++ InventoryAccounting/dbo/Accounting.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\nasur\source\repos\InventoryAccounting\InventoryAccounting\dbo\Accounting.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\mamon\OneDrive\Рабочий стол\InventoryAccounting-master\InventoryAccounting\dbo\Accounting.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
